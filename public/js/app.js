497cdb1d0c1d9b8e70d561850bb4a890ac010032
Queen <youngidia@live.com>
2019-03-03 15:59:14 -0500

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,60 +1,71 @@
-$(function() {
+//$(function() {
 
-  const render = function(items) {
-    items.forEach(function(item) {
-      $("#sale-items").append(buildRows(item));
-    })
-  };
+const render = function(items) {
+  items.forEach(function(item) {
+    $("#sale-items").append(buildRows(item));
+  });
+};
 
-  const getProduct = function() {
-    $.get("/api/Product").then(render);
-  };
+const getProduct = function() {
+  $.get("/api/Product").then(render);
+};
 
-  getProduct();
+getProduct();
 
-  const buildRows = function(item) {
-    const tr = $("<tr>")
+const buildRows = function(item) {
+  const tr = $("<tr>");
 
-    tr.append(
-      $('<input id="editbox1"  size="5" type="text">'),
-      $('<td>').text(item.product_name),
-      $('<td>').text(item.department_name),
-      $('<td>').text(item.price),
-      $('<td>').text(item.stock_quantity),
-      $('<input id="editbox2"  size="8" type="text">')
+  tr.append(
+    $("<form>"),
+    $('<input id="editbox1"  size="5" type="text">'),
+    $("<td>").text(item.product_name),
+    $("<td>").text(item.department_name),
+    $("<td>").text(item.price),
+    $("<td>").text(item.stock_quantity),
+    $('<input id="editbox2"  size="8" type="text">'),
+    $("<td>").text(" "),
+    $('<input id="updatePrice" type = "submit" value="update"></input>'),
+    $("<td>").text(" "),
+    $('<input id="addToCart" type = "submit" value="Add to Cart"></input>'),
+    $("</form>")
+  );
+  return tr;
+};
 
-            
-    )
-    return tr
-  };
- function calculate(){
- // let editbox = document.getElementById("editbox1").value;
- let input = editbox1;
-  let price = item.price;
-  let multiply = input * parseFloat(price);
-  console.log(multiply);
- }
- $('#shop').on('click', calculate );
- 
+function updatePrice() {
+  let quantity = document.getElementById("editbox1").value;
+  let totalPrice = parseInt(quantity) * parseFloat(item.price);
+  console.log(totalPrice);
+  $("#editbox2").append(totalPrice);
+}
+updatePrice()//calculate();
 
-  
-  // POST the product object to /api/product
-   $.post("/api/Product", product)
+function addToCart() {
+  console.log("adding to cart");
+}
+
+//calculatee();
+
+//POST the product object to /api/product
+$.post("/api/Product", product)
    .then(function(data) {
     res.json(data);
   })
   .catch(function (err) {
     res.json(err);
   });
-  // After receiving a response, call get product
-  getProduct();
+// After receiving a response, call get product
+//
 
-  function validateForm() {
+/*function validateForm() {
     var x = item.stock_quantity;
     if (x < editbox1) {
       alert("Insufficient quantity!");
       return false;
     }
   }
-  validateForm();
-});
+  validateForm();*/
+//});
+
+$("#shop").on("click", addToCart);
+$("#updatePrice").on("click", updatePrice);
